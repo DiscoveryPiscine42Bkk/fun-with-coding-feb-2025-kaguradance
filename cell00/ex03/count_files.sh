c61393abffdd12655fbc8d73bdd0ffdf4ba6bab8
kaguradance <pimsirin.kome@gmail.com>
2025-02-06 22:47:39 +0700

    00 03

---
+++ cell00/ex03/count_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Count regular files and directories
+file_count=$(find . -maxdepth 1 -type f | wc -l)
+dir_count=$(find . -maxdepth 1 -type d | wc -l)
+
+echo "$file_count$dir_count"
